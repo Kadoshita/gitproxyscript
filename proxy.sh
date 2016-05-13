fc1304722b0c1fc7962f2dd9c8d3578fc4e51e50
Kadoshita <kadoshita.inct@gmail.com>
2016-05-13 13:41:21 +0900

    edited proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -6,7 +6,7 @@ if [ $1 = $unset ]; then
 	git config --global --unset http.proxy
 	echo $unset
 elif [ $1 = $set ]; then
-	git config --global http.proxy http://202.211.7.4:8080
-	git config --global https.proxy http://202.211.7.4:8080
+	git config --global http.proxy http://#自分の環境のプロキシサーバ―のアドレス:プロキシポート
+	git config --global https.proxy http://#自分の環境のプロキシサーバ―のアドレス:プロキシポート
 	echo $set
 fi
